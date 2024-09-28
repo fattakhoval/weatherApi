c523bdba8aff6806866ecd3666f97f5ab7f32883
fattakhoval <148847974+fattakhoval@users.noreply.github.com>
2024-09-28 07:16:53 +0300

    Погода для 5 городов
    Обновлены функции для вывода погоды в выбранном городе

--- get_weather.php
+++ get_weather.php
@@ -2,15 +2,21 @@
 // Ваш API-ключ
 $apiKey = "ba23f7f55ac93655a80738bf4d41aa81";
 
-// ID города Уфа (у OpenWeatherMap у каждого города есть свой ID)
-$cityId = "479561"; // ID для Уфы
 
-// Формируем URL для запроса к API OpenWeatherMap
-$url = "http://api.openweathermap.org/data/2.5/weather?id={$cityId}&units=metric&lang=ru&appid={$apiKey}&lang=ru";
+function getWeather($cityId){
+    global $apiKey;
+    $url = "http://api.openweathermap.org/data/2.5/weather?id={$cityId}&units=metric&lang=ru&appid={$apiKey}&lang=ru";
 
 // Выполняем запрос к API
 $response = file_get_contents($url);
 
 // Выводим полученные данные
-echo $response;
+return $response;
+
+}
+// (у OpenWeatherMap у каждого города есть свой ID)
+$cityId = $_GET['city_id'];
+
+echo getWeather($cityId);
+
 ?>
